14ce5b41e57b0b6eff50e0a52f2f6906f1677319
PabloRab <142822900+PabloRab@users.noreply.github.com>
2024-06-24 01:46:32 +0300

    hw1

---
+++ home-work1/script.js
@@ -0,0 +1,47 @@
+// 1
+const age= 16;
+console.log(age);
+
+// 2
+const userName = "Kirill";
+console.log(userName);
+
+// 3
+const isStudent = true;
+console.log(isStudent);
+
+// 4
+const isString = "Phantom4s"
+console.log(isString);
+
+// 5
+let myNumber = 10;
+myNumber += 10;
+console.log(myNumber);
+
+// 6
+const myNull = null;
+console.log(myNull);
+
+// 7
+// const name = prompt('Еnter your name');
+// alert('Welcome, ' + name + '!');
+
+// 8
+// const nameUser = confirm('Ви підтверджуєте дію?');
+// if(nameUser){
+//     alert('Дякую!')
+// }
+// else{
+//     alert('Дію відмінено')
+// }
+
+// 9
+// alert('Увага дія небезпечна!');
+// const dangerous = confirm('Ви підтверджуєте дію?');
+// if(dangerous){
+//     alert('Дякую за пітвердження!')
+// }
+// else{
+//     alert('Дію відміненно')
+// }
