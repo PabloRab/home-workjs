6b26f800257238e38eec72ab2a5c82cbb15a905c
PabloRab <142822900+PabloRab@users.noreply.github.com>
2024-06-30 21:54:39 +0300

    /

---
+++ home-work2/script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// 1
+const c = 20;
+const f = 1.8 * c + 32;
+console.log(f);
+
+// 2
+const days = 30;
+const hours = days * 24;
+const minutes = hours * 60;
+console.log(hours);
+console.log(minutes);
+
+// 3
+let h = 100;
+let e = 100;
+h -= 25;
+e -= 30;
+console.log(h);
+console.log(e);
+
+// 4
+let price = 100;
+let sum = price - price * 0.1;
+console.log(sum)
+
+// 5
+const n = 3.14;
+console.log(Math.floor(n))
+
+// 6
+const x = '5.741hg11';
+console.log(parseFloat(x))
+
+// 7
+const v = '3.1524pppr1'
+console.log(parseInt(v))
+
+// 8
+const a = 16;
+console.log(Math.sqrt(a));
+
+// 9
+const num = 20;
+const string = '15kf';
+console.log(parseInt(string));
+console.log(num.toString());
+console.log(typeof num.toString());
