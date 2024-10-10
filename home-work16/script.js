c722af7d92b888b1eae440d11820ba3969b108ab
PabloRab <142822900+PabloRab@users.noreply.github.com>
2024-10-10 17:14:04 +0300

    .

---
+++ home-work16/script.js
@@ -0,0 +1,55 @@
+// //Task 1
+// class Account {
+//     constructor({email, login}) {
+//         this.email = email;
+//         this.login = login;
+//     }
+//     getInfo() {
+//         return `login: ${this.login}, email: ${this.email}`;
+//     }
+// }
+
+
+// console.log(Account.prototype.getInfo); // function
+
+// const mango = new Account({
+//  login: 'Mangozedog',
+//  email: 'mango@dog.woof',
+// });
+
+// mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+// const poly = new Account({
+//  login: 'Poly',
+//  email: 'poly@mail.com',
+// });
+
+// poly.getInfo(); // Login: Poly, Email: poly@mail.com
+
+//task 2
+// class User{
+//     constructor({ name, age, followers}) {
+//         this.name = name;
+//         this.age = age;
+//         this.followers = followers;
+//     }
+//     getInfo() {
+//         return console.log(`User ${this.name} is ${this.age} years old and has ${this.followers} followers`);
+//     }
+// }
+
+// const mango = new User({
+//     name: 'Mango',
+//     age: 2,
+//     followers: 20,
+//    });
+   
+//    mango.getInfo(); // User Mango is 2 years old and has 20 followers
+   
+//    const poly = new User({
+//     name: 'Poly',
+//     age: 3,
+//     followers: 17,
+//    });
+   
+//    poly.getInfo(); // User Poly is 3 years old and has 17 followers
